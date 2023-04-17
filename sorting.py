7fd12e2904498dce33034a64f7f7aec6418755ae
Karolina Sedova <sedovakarolina@gmail.com>
2023-04-17 12:18:28 +0200

    Add function for selection sort

--- sorting.py
+++ sorting.py
@@ -23,10 +23,22 @@ def read_data(file_name):
 
         return data
 
+def selection_sort(number_array):
+    for i in range(len(number_array) - 1):
+        min_idx = i
+        for num_idx in range(i + 1, len(number_array)):
+            if number_array[min_idx] > number_array[num_idx]:
+                min_idx = num_idx
+        number_array[i], number_array[min_idx] = number_array[min_idx], number_array[i]
+
+    return number_array
+
+
 def main():
     data = read_data('numbers.csv')
     print(data)
-
+    sorted_array = selection_sort(data['series_2'].copy())
+    print(sorted_array)
 
 if __name__ == '__main__':
     main()
